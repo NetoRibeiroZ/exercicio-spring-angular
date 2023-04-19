e466c109c4675845435eca6b91898f30aeb0979e
Neto Rodrigues <antoniodrigues@hotmail.com>
2023-04-19 00:49:49 -0300

    Adicionando documentação swagger

--- backed/src/main/java/exercicio/java/angular/backend/documentos/controller/DocumentoController.java
+++ backed/src/main/java/exercicio/java/angular/backend/documentos/controller/DocumentoController.java
@@ -2,6 +2,8 @@ package exercicio.java.angular.backend.documentos.controller;
 
 import exercicio.java.angular.backend.documentos.model.Documento;
 import exercicio.java.angular.backend.documentos.service.IDocumentoService;
+import io.swagger.annotations.ApiOperation;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -17,7 +19,9 @@ public class DocumentoController {
     @Autowired
     private IDocumentoService service;
 
+
     @GetMapping
+    @ApiOperation(value = "Lista todos os documentos")
     public List<Documento> listAll(
             @PathVariable Long setorId,
             @PathVariable Long pastaId,
@@ -26,6 +30,7 @@ public class DocumentoController {
     }
 
     @GetMapping("{id}")
+    @ApiOperation(value = "Pesquisa Pelo Id de pastas e setores")
     public Documento findById(
             @PathVariable Long setorId,
             @PathVariable Long pastaId,
@@ -35,6 +40,7 @@ public class DocumentoController {
     }
 
     @PostMapping
+    @ApiOperation(value = "Insere um novo documento no banco")
     public Documento insert(
             @PathVariable Long setorId,
             @PathVariable Long pastaId,
@@ -43,6 +49,7 @@ public class DocumentoController {
     }
 
     @PutMapping
+    @ApiOperation(value = "Atualiza um documento no  banco")
     public Documento update(
             @PathVariable Long setorId,
             @PathVariable Long pastaId,
--- backed/src/test/java/exercicio/java/angular/backend/BackendApplicationTests.java
+++ backed/src/test/java/exercicio/java/angular/backend/BackendApplicationTests.java
@@ -1,5 +1,4 @@
 package exercicio.java.angular.backend;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
